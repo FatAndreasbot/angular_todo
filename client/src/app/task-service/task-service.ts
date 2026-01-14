63e2cd2cfb0673aacfc75b66d70bf5a3fb68bbc2
andrew <guva.andrej.01@gmail.com>
2026-01-14 17:23:48 +0700

    added some functionalyity to service

--- client/src/app/task-service/task-service.ts
+++ client/src/app/task-service/task-service.ts
@@ -1,8 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Task } from '../task';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
-  url: string = "http://localhost:8000"  
+  private url: string = "http://localhost:8000"
+  
+  constructor (
+    private http: HttpClient
+  ) { }
+
+  getAllTasks():Observable<Task[]>{
+    return this.http.get<Task[]>(this.url)
+  }
+
+  getTaskById(id:number):Observable<Task>{
+    const urlWithParams = new URL(this.url)
+    urlWithParams.searchParams.append("id", String(id))
+
+    const resp = this.http.get<Task>(urlWithParams.href)
+    return resp
+  }
 }
