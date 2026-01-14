a1668741863dbf2a935825fefc1ce06e95500735
andrew <guva.andrej.01@gmail.com>
2026-01-14 15:52:17 +0700

    basic initial editing"

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AllTasks } from './all-tasks/all-tasks';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        title:'all tasks',
+        component: AllTasks
+    }
+];
